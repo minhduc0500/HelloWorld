43f22bfc6f39e6b487ceef06cdde6170ac3b7f84
Duc <https://github.com/minhduc0500/HelloWorld>
2018-08-22 15:34:17 +0700

    sinhvien

---
+++ src/helloword/sinhvien.java
@@ -0,0 +1,14 @@
+package helloword;
+
+public class sinhvien
+    {
+	public static void main(String[] agrs)
+	{
+		sinhvien sv= new sinhvien();
+		sv.hoten="minh duc";
+		sv.masv=123;
+		sv.thi();
+		sv.donghocphi();
+		
+		}
+    }
